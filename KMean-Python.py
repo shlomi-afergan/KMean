1e6c46f0c891799484bc40937add077e58c1e73f
idoam <idoamit1@mail.tau.ac.il>
2022-04-03 19:20:43 +0300

    first commit

--- KMean-Python.py
+++ KMean-Python.py
@@ -1,10 +1,24 @@
+import sys
+
+args = sys.argv
+k = int(args[1])
+if len(args) == 5:
+    iterations = int(args[2])
+    inputFileName = args[3]
+    outputFileName = args[4]
+else:
+    iterations = 200
+    inputFileName = args[2]
+    outputFileName = args[3]
+
+
 # Calculate the euclidean distance between two vectors.
-def euclideanDistance(vec1: list, vec2: list) -> int:
+def euclideanDistance(vec1: list[float], vec2: list[float]) -> float:
     return (sum([(vec1[i] - vec2[i]) ** 2 for i in range(len(vec1))])) ** 0.5
 
 
 # Calculate cluster centroid.
-def clusterCentroid(vectors: list[list]) -> list[float]:
+def clusterCentroid(vectors: list[list[float]]) -> list[float]:
     vec_len = len(vectors[0])
     centroid = []
     for i in range(vec_len):
@@ -12,16 +26,14 @@ def clusterCentroid(vectors: list[list]) -> list[float]:
         for lst in vectors:
             summ += lst[i]
         center = summ / len(vectors)
-        center = "%.4f" % center
-        center = float(center)
+        center = round(center, 4)
         centroid.append(center)
-        # append(summ / len(vectors))
     return centroid
 
 
-def clustersFar(old: list[list], new: list[list]) -> bool:
+def clustersFar(old: list[list[float]], new: list[list[float]]) -> bool:
     for i in range(len(old)):
-        if euclideanDistance(old[i], new[i]) > 0.01:
+        if euclideanDistance(old[i], new[i]) > 0.001:
             return True
     return False
 
@@ -39,17 +51,12 @@ def addZero(num: str):
     return ".".join(numLst)
 
 
-print(clusterCentroid([[0, 0], [4, 4], [0, 4], [4, 0]]))  # -----------------Code Review---------------------------
-print(clustersFar([[0, 0], [4, 4], [0, 4], [4, 0]], [[0,3], [3,4], [2,2], [1,4]]))
-
-print(euclideanDistance([7, 11], [11, 8]))  # -----------------------------Code Review-----------------------------
-
-
-def kMeans(k: int, myInput: str, max_iter=200):
+def kMeans(k: int, max_iter: int, myInput: str, myOutput: str):
     """
     :param k: The number of desired clusters.
     :param myInput: A text file contains all the points in R**d.
     :param max_iter: max iterations allowed.
+    :param myOutput: txt file to output.
     :return: K centers of points.
     """
     # process input
@@ -60,16 +67,12 @@ def kMeans(k: int, myInput: str, max_iter=200):
             inputLst.append(lst)
     f.close()
 
-    #  print(inputLst)  # -----------------------------Code Review---------------------------------------
-
-    # Initialize cluster centers.
+    # Initialize clusters centers.
     centroids = [inputLst[i] for i in range(k)]
-    # print("centroids are: ", centroids)  # -----------------------------Code Review---------------------------------
 
     counter = 0
 
     # Repeat until convergence.
-    # new_centroids = [[float("inf") for x in vec] for vec in centroids]
     new_centroids = [[float("inf") for x in lst] for lst in centroids]
 
     while counter < max_iter and clustersFar(centroids, new_centroids):
@@ -87,25 +90,25 @@ def kMeans(k: int, myInput: str, max_iter=200):
                     cluster_idx = i
                     min_euc = tmp
             clusters[cluster_idx].append(vec)
+
         # Update centroids.
         for i in range(len(clusters)):
             new_centroids[i] = clusterCentroid(clusters[i])
-        # print("cent; ", centroids)
-        # print("new cent: ", new_centroids)
 
-        # update counter
-        counter += 1
+        counter += 1  # update counter
     # output result
     strLst = toStrLst(new_centroids)
-    with open('myFile.txt', 'w') as f:
+    with open(outputFileName, 'w') as f:
         for line in strLst:
             f.write(line)
             f.write('\n')
-    print("result is: ", new_centroids)
     return new_centroids
 
 
-m = kMeans(3, "input_1.txt")  # -----------------------------Code Review---------------------------------------
+kMeans(k, iterations, inputFileName, outputFileName)
 
 
-print(toStrLst(m))
+
+
+
+
